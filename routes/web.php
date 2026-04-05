852b4a62925b0706f1f9907b007cf94eb8a9f1e8
Latifa270 <latifajauharatul4@gmail.com>
2026-04-05 15:27:57 +0700

    halaman data pengguna

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use App\Http\Controllers\Admin\BookingController;
 use App\Http\Controllers\Admin\GalleryController;
 use App\Http\Controllers\Admin\GameController;
 use App\Http\Controllers\Admin\LaporanController;
+use App\Http\Controllers\SuperadminController;
+use App\Http\Controllers\DataUserController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -35,6 +38,8 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middl
 Route::middleware(['auth', RoleMiddleware::class.':superadmin'])->prefix('superadmin')->name('superadmin.')->group(function () {
     Route::get('/dashboard', fn () => view('superadmin.dashboard'))->name('dashboard');
 });
+    //data user
+    Route::get('/superadmin/datauser', [DataUserController::class, 'index'])->name('superadmin.datauser');
 
 // Admin
 Route::middleware(['auth', RoleMiddleware::class.':admin'])->prefix('admin')->name('admin.')->group(function () {
