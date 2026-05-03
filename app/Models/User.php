2aa6c731ac20762ac63fa1aee57eaa237c9a1885
Latifa270 <latifajauharatul4@gmail.com>
2026-05-03 12:23:33 +0700

    register

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,6 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(TrTransaksi::class, 'id_pengguna', 'id_pengguna');
     }
-
         
     public function getAuthIdentifierName()
     {
