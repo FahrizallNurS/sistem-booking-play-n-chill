8041d09c5df4c9dc268057107bbfb2111e1a4b97
FahrizallNurS <rizalkhadam@gmail.com>
2026-05-10 12:04:35 +0700

    feat : laporan admin

--- app/Http/Controllers/Admin/LaporanController.php
+++ app/Http/Controllers/Admin/LaporanController.php
@@ -3,11 +3,74 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\TrTransaksi;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class LaporanController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.laporan.index');
+        $periode       = $request->input('periode', 'harian');
+        $statusBooking = $request->input('status_booking', '');
+        $statusBayar   = $request->input('status_bayar', '');
+        $jenisBayar    = $request->input('jenis_bayar', '');
+
+        // Tentukan range tanggal berdasarkan periode
+        switch ($periode) {
+            case 'mingguan':
+                $minggu = $request->input('minggu', now()->format('Y-W'));
+                [$year, $week] = explode('-W', $minggu);
+                $start = Carbon::now()->setISODate($year, $week)->startOfWeek();
+                $end   = Carbon::now()->setISODate($year, $week)->endOfWeek();
+                break;
+
+            case 'bulanan':
+                $bulan = $request->input('bulan', now()->format('Y-m'));
+                $start = Carbon::parse($bulan . '-01')->startOfMonth();
+                $end   = Carbon::parse($bulan . '-01')->endOfMonth();
+                break;
+
+            default: // harian
+                $tanggal = $request->input('tanggal', now()->format('Y-m-d'));
+                $start   = Carbon::parse($tanggal)->startOfDay();
+                $end     = Carbon::parse($tanggal)->endOfDay();
+                break;
+        }
+
+        $query = TrTransaksi::with(['pengguna', 'penetapanHarga.ruangan', 'penetapanHarga.paket'])
+            ->whereBetween('waktu_mulai', [$start, $end]);
+
+        if ($statusBooking) {
+            $query->where('status_sewa', $statusBooking);
+        }
+
+        if ($statusBayar) {
+            $query->where('status_pembayaran', $statusBayar);
+        }
+
+        if ($jenisBayar) {
+            $query->where('opsi_pembayaran', $jenisBayar);
+        }
+
+        $transaksis = $query->latest('waktu_mulai')->get();
+
+        // Summary
+        $totalBooking    = $transaksis->count();
+        $totalSelesai    = $transaksis->where('status_sewa', 'selesai')->count();
+        $totalDibatalkan = $transaksis->where('status_sewa', 'dibatalkan')->count();
+        $totalPendapatan = $transaksis->whereIn('status_sewa', ['selesai'])
+                            ->sum('total_harga');
+
+        return view('admin.laporan.index', compact(
+            'transaksis',
+            'totalBooking',
+            'totalSelesai',
+            'totalDibatalkan',
+            'totalPendapatan',
+            'periode',
+            'start',
+            'end'
+        ));
     }
 }
